41a341454f09ce97973e657fb369c327650e2373
Yalin <yalin.gunayer@gmail.com>
2019-07-04 19:28:19 +0300

    Add comments for distributed example

--- distributed/bar.erl
+++ distributed/bar.erl
@@ -2,8 +2,12 @@
 -export([start/0]).
 
 start() ->
+    % notice how we can transparently send our `Pid` to remote node
+    % without having to specify anything
     {foo_handler, 'foo@erlang.example'} ! {self(), hello},
     io:format("Sent hello to foo_handler at foo@erlang.example~n"),
     receive
-        {goodbye} -> io:format("bar received `goodbye` and will terminate~n"), self() ! stop
+        {goodbye} ->
+            io:format("bar received `goodbye` and will terminate~n"),
+            self() ! stop
     end.
--- distributed/foo.erl
+++ distributed/foo.erl
@@ -5,5 +5,9 @@ start() ->
     register(foo_handler, self()),
     io:format("foo is now registered with the name `foo_handler`~n"),
     receive
-        {Sender, hello} -> io:format("foo received `hello` and will terminate after replying `goodbye` back~n"), Sender ! {goodbye}, self() ! stop
+        {Sender, hello} ->
+            io:format("foo received `hello` and will terminate after replying `goodbye` back~n"),
+            Sender ! {goodbye}, % reply to sender, notice how we don't have to worry about
+                                % whether or not the sender is on a separate Erlang node
+            self() ! stop
     end.
