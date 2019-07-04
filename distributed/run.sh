54007441948813bc28dd7ddb6355c792bfb12bd3
Yalin <yalin.gunayer@gmail.com>
2019-07-04 19:28:37 +0300

    Fix run script for distributed example

--- distributed/run.sh
+++ distributed/run.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
+set -e
 
-IP=$(getent hosts erlang.example | head -n 1 | awk '{print $1}')
+case "$OSTYPE" in
+    darwin*) IP=$(dscacheutil -q host -a name erlang.example | grep ip_address | head -n 1 | awk '{print $2}') ;;
+    solaris|linux|msys|bsd*) IP=$(getent hosts erlang.example | head -n 1 | awk '{print $1}') ;;
+    *) echo "Cannot retrieve IP for hostname erlang.example" && exit 1 ;;
+esac
 
 if [ "$IP" != "127.0.0.1" ]; then
     echo "The hostname erlang.example seems to point to $IP instead of 127.0.0.1"
     read -p "Are you sure you want to continue? (y/N) " REPLY
-    if [[ "$REPLY" =~ ^[Yy]$ ]]; then
-        rm *.beam
-        erl -compile foo
-        erl -compile bar
-        erl -noshell -name foo@erlang.example -s foo -s init stop & \
-        erl -noshell -name bar@erlang.example -s bar -s init stop
+    if [[ ! "$REPLY" =~ ^[Yy]$ ]]; then
+        exit 1
     fi
 fi
+
+rm -f *.beam
+erl -compile foo
+erl -compile bar
+erl -noshell -name foo@erlang.example -s foo -s init stop & \
+erl -noshell -name bar@erlang.example -s bar -s init stop
